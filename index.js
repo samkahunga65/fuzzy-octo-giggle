e7981b4f95eb587d907f87e8846be17e43028246
kahunga <samkahunga65@gmail.com>
2023-05-14 17:45:22 +0300

    s

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import { Server } from "socket.io";
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  // options
+  cors: {
+    origins: "*:*",
+    methods: ["GET", "POST"]
+  }
 });
 
 io.on("connection", (socket) => {
