a1890e2beed6b5df814b82aac5de55207f3e7aa2
samkahunga65 <samkahunga65@gmail.com>
2023-05-14 17:46:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 
 const httpServer = createServer();
-const io = new Server(httpServer,{
+const io = new Server(httpServer, {
   cors: {
     origins: "*:*",
     methods: ["GET", "POST"]
-  });
+  }
+});
 
 io.on("connection", (socket) => {
   console.log('conn');
