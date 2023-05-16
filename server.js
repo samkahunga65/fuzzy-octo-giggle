d45e217e13512cb29ba2a43c92f16b67679ca705
samkahunga65 <samkahunga65@gmail.com>
2023-05-16 11:13:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-  console.log("on")
+  console.log("I be on")
 
 io.on("connection", socket => {
   console.log("connected")
