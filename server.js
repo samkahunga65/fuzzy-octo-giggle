1b1d772d6c6237fcbc033cccd7085468ac59d9e7
Glitch (thundering-adaptive-debt) <none>
2023-05-16 08:06:57 +0000

    🪒🎱 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+  console.log("connected")
+
+io.on("connection", socket => {
+  console.log("connected")
+  socket.on('qq', ()=>{
+    console.log('qq');
+  })
+  socket.on('disconnect', ()=>{
+    console.log('disconnect');
+  })
+});
+
+server.listen(process.env.PORT, () =>
+  console.log("server listening on " + process.env.PORT)
+);
